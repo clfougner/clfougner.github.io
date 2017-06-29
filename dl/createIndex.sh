fd1d95e52e93baccfae41017ef63cf7d79603db8
Christian Fougner <clfougner@users.noreply.github.com>
2017-06-29 19:51:24 +0200

    Fixed downloads page

--- dl/createIndex.sh
+++ dl/createIndex.sh
@@ -1,1 +1,1 @@
-ls | perl -e 'print "<html><body><h1>Downloads</h1><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
+ls | grep -v "index.html" | grep -v "createIndex.sh" | perl -e 'print "<html><body><h1>Downloads</h1><ul>"; while(<>) { chop $_; print "<li><a href=\"./$_\">$_</a></li>";} print "</ul></body></html>"' > index.html
